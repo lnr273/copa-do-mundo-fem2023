460df8a79f7ba96b48294126a999a1abbf72b2a6
Luan Reis <luanreis2723@gmail.com>
2024-08-10 10:16:38 -0300

    Criação do componente Card para cada grupo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Card from './components/Card/Card.jsx';
+
+function App() {
+
+  return (
+    <>
+      <h1>Copa do Mundo Feminina 2023</h1>
+      <section className='cards'>
+        <Card />
+      </section>
+    </>
+  );
+}
+
+export default App;
